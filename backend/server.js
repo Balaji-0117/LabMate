2e57a5ec829a3fd04dcb5e0284b3055c156f2c81
Balaji-0117 <balajibal2006@gmail.com>
2026-04-04 00:10:48 +0530

    Setup basic server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const app = express();
 
+//middleware
+app.use(express.json());
+
+// Defining a simple route
+app.get("/", (req,res)=> {
+    res.send("LabMate Server is Running!");
+})
+
+const PORT = 3000;
+app.listen(PORT, (req,res)=> {
+    console.log(`Server is Running on http://localhost:${PORT}`);
+})
